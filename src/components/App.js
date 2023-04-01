baa937b65cdd79e31859cf329231742ab332fea5
Buse Ozkan <buse1.ozkan@gmail.com>
2023-04-01 18:33:36 +0100

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import "../styles/App.css";
 import React, { useEffect, useState } from "react";
 import Weather from "./Weather";
 import { Dimmer, Loader } from "semantic-ui-react";
-
+import Forecast from "./forecast";
+import Search from "./search";
+import CurrentWeather from "./getWeather";
 function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
   const [data, setData] = useState([]);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,14 +25,37 @@ function App() {
         .then((res) => res.json())
         .then((result) => {
           setData(result);
-          console.log(result);
         });
     };
     fetchData();
   }, [lat, long]);
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${process.env.REACT_APP_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forcastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
+      })
+      .catch(console.log);
+  };
 
   return (
     <div className="App">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
+
       {typeof data.main != "undefined" ? (
         <Weather weatherData={data} />
       ) : (
---
+++ src/components/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c0a03b7c22mshff9ab8f6fe40e3bp163a41jsn3007eab6fd0a", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
